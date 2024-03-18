49d052d80f816c87dcf0d824a5f9a86596f6d8da
thaydeveloper <thaydeveloper26@gmail.com>
2024-03-18 09:47:39 -0300

    integracao feita com api coingecko

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useApi } from "./services/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const api = useApi();
+
+  const hadleGetCoinsList = async () => {
+    const response = await api.getListCoinsCryptoCurrencies();
+    console.log(response);
+  };
+  useEffect(() => {
+    hadleGetCoinsList();
+  }, []);
 
   return (
     <>
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
